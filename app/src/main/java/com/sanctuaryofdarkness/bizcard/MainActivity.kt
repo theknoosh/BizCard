6d757af076d6ed24994571b5c82258082075ea44
Darrell Payne <darrellpayne@earthlink.net>
2022-02-13 19:12:56 -0800

    Completed part 15, Section 6

--- app/src/main/java/com/sanctuaryofdarkness/bizcard/MainActivity.kt
+++ app/src/main/java/com/sanctuaryofdarkness/bizcard/MainActivity.kt
@@ -3,7 +3,10 @@ package com.sanctuaryofdarkness.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -13,6 +16,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sanctuaryofdarkness.bizcard.ui.theme.BizcardTheme
@@ -33,16 +38,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    androidx.compose.material.Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Card(modifier = Modifier
-            .width(200.dp)
-            .height(390.dp)
-            .padding(18.dp),
+    androidx.compose.material.Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(18.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-        elevation = 6.dp) {
-
+            elevation = 6.dp
+        ) {
+            androidx.compose.material.Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                elevation = 4.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+            ) {
+                Image(painter = painterResource(id = R.drawable.placeholder),
+                    contentDescription = "profile image",
+                modifier = Modifier.size(135.dp),
+                contentScale = ContentScale.Crop)
+            }
         }
     }
 }
