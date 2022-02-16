59069838c5a125a7f5c39903f5daa3a1d0e10a09
Darrell Payne <darrellpayne@earthlink.net>
2022-02-16 13:07:14 -0800

    Completed part 20, Section 6

--- app/src/main/java/com/sanctuaryofdarkness/bizcard/MainActivity.kt
+++ app/src/main/java/com/sanctuaryofdarkness/bizcard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sanctuaryofdarkness.bizcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -36,7 +37,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    androidx.compose.material.Surface(
+    Surface(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
@@ -49,38 +50,49 @@ fun CreateBizCard() {
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = 6.dp
         ) {
-            CreateInfo()
+
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider()
+                CreateInfo()
+
+                Button(onClick = {
+                    Log.d("BizCardMessage", "CreateBizCard: Clicked!!")
+                }) {
+                    Text(
+                        "Portfolio",
+                        style = MaterialTheme.typography.button
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
 private fun CreateInfo() {
-    Column(
-        modifier = Modifier.height(300.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        CreateImageProfile()
-        Divider()
-        Column(modifier = Modifier.padding(5.dp)) {
-            Text(
-                "Sarah P.",
-                style = MaterialTheme.typography.h4,
-                color = MaterialTheme.colors.primary
-            )
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            "Sarah P.",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primary
+        )
 
-            Text(
-                "Android Compose Programmer",
-                modifier = Modifier.padding(3.dp)
-            )
+        Text(
+            "Android Compose Programmer",
+            modifier = Modifier.padding(3.dp)
+        )
 
-            Text(
-                "@darrellComposer",
-                modifier = Modifier.padding(3.dp),
-                style = MaterialTheme.typography.subtitle2
-            )
-        }
+        Text(
+            "@darrellComposer",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle2
+        )
+
     }
 }
 
