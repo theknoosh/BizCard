26499ad5487fe2247b842446e8087ecf641ae6f8
Darrell Payne <darrellpayne@earthlink.net>
2022-02-16 18:06:29 -0800

    Completed part 23 of section 6

--- app/src/main/java/com/sanctuaryofdarkness/bizcard/MainActivity.kt
+++ app/src/main/java/com/sanctuaryofdarkness/bizcard/MainActivity.kt
@@ -7,11 +7,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,6 +41,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+
+    val buttonClickedstate = remember {
+        mutableStateOf(false)
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -61,12 +70,18 @@ fun CreateBizCard() {
                 CreateInfo()
 
                 Button(onClick = {
-                    Log.d("BizCardMessage", "CreateBizCard: Clicked!!")
+                    buttonClickedstate.value = !buttonClickedstate.value // toggle
                 }) {
                     Text(
                         "Portfolio",
-                        style = MaterialTheme.typography.button
-                    )
+                        style = MaterialTheme.typography.button)
+                }
+                if(buttonClickedstate.value) {
+                    Content()
+                } else {
+                    Box() {
+
+                    }
                 }
             }
         }
@@ -97,7 +112,12 @@ fun Content() {
 
 @Composable
 fun Portfolio(data: List<String>) {
-    Text("Projects go here")
+    
+    LazyColumn{
+        items(data) {item ->
+            Text(item)
+        }
+    }
 }
 
 @Composable
